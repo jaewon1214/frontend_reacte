635d08ad9d480cde77d33aa45e1c1cf71a2c03d7
jaewon1214 <viver4454@gmail.com>
2026-05-14 21:22:01 +0900

    onchange_onsubmiit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import './App.css'
+import { useState } from 'react'
+import ScoresTable from './components/ScoresTable'
 
 const style = {
   width: "500px",
@@ -9,18 +11,51 @@ const style = {
   alignItems: "center"
 }
 
+const initscores = [
+  { 이름 : 'john', 국어 : 91, 영어 : 91, 수학 : 81, 과학 : 91},
+  { 이름 : 'peter', 국어 : 92, 영어 : 92, 수학 : 82, 과학 : 92},
+  { 이름 : 'susan', 국어 : 93, 영어 : 93, 수학 : 83, 과학 : 93},
+  { 이름 : 'sue', 국어 : 94, 영어 : 94, 수학 : 84, 과학 : 94},
+
+]
+
+const initinfo = {
+  이름 : "",
+  국어 : null,
+  영어 : null,
+  수학 : null,
+  과학 : null,
+}
 
 function App() {
+  const [info, setA] = useState(initinfo)
+  const [scores, setScores] = useState(initscores)
+
   const handleChange = (event) =>{
+    const {name, value} = event.target;
+    setA((prev)=>(
+        {...prev, [name]: value} // {...prev, [event.target.name]: event.target.value}
+    ))
     console.log(event.target.name)
   }
+  const handlesubmit = (evant) =>{
+    evant.preventDefault();
+   // console.log(evant.target)
+    setScores((prev)=>(
+      [...prev, info]
+    ))
+  }
   return (
-    <form>
+    <>
+
+    <ScoresTable scores={scores}/>
+
+     <form onSubmit={handlesubmit}>
       <div>이름 : 
         <input 
           type="text" 
           name="이름" 
-          value=""
+          value={info.이름}
           onChange={handleChange}
         /> 
       </div>
@@ -28,7 +63,7 @@ function App() {
         <input 
           type="number" 
           name="국어" 
-          value="" 
+          value={info.국어} 
           onChange={handleChange}
           /> 
         </div> 
@@ -36,7 +71,7 @@ function App() {
         <input 
           type="number" 
           name="영어" 
-          value="" 
+          value={info.영어} 
           onChange={handleChange}
           /> 
         </div> 
@@ -44,7 +79,7 @@ function App() {
         <input 
           type="number" 
           name="수학" 
-          value="" 
+          value={info.수학}
           onChange={handleChange}
           /> 
         </div> 
@@ -52,12 +87,15 @@ function App() {
         <input 
           type="number" 
           name="과학" 
-          value="" 
+          value={info.과학} 
           onChange={handleChange}
           /> 
         </div> 
       <button>제출</button>
     </form>
+    
+    </>
+   
   )
 }
 
--- src/components/ScoresTable.jsx
+++ src/components/ScoresTable.jsx
@@ -1,27 +1,27 @@
 import React from 'react'
 
-const scores = [
+/*const scores = [
   { 이름 : 'john', 국어 : 91, 영어 : 91, 수학 : 81, 과학 : 91},
   { 이름 : 'peter', 국어 : 92, 영어 : 92, 수학 : 82, 과학 : 92},
   { 이름 : 'susan', 국어 : 93, 영어 : 93, 수학 : 83, 과학 : 93},
   { 이름 : 'sue', 국어 : 94, 영어 : 94, 수학 : 84, 과학 : 94},
-]
+]*/
 
-function scoresTable() {
+function scoresTable({scores}) {
   return (
      <table>
       <tr>
         {
-          Object.keys(scores[1]).map(key=>(
+          Object.keys(scores[0]).map(key=>(
             <th>{key}</th>
           ))
         }
       </tr>
-      {scores.map(item =>
+      {scores.map(abx =>
           (
           <tr>
             {
-              Object.values(item).map(
+              Object.values(abx).map(
                 value => (<td>{value}</td>)
               )
             }
