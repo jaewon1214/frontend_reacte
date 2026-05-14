1ffcd7c23d25efa6850ff42dafaf657b7878ec79
jaewon1214 <viver4454@gmail.com>
2026-05-14 15:40:24 +0900

    4th

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
 import './App.css'
 
-const scores = [
-  { 이름 : 'john', 국어 : 91, 영어 : 91, 수학 : 81, 과학 : 91},
-  { 이름 : 'peter', 국어 : 92, 영어 : 92, 수학 : 82, 과학 : 92},
-  { 이름 : 'susan', 국어 : 93, 영어 : 93, 수학 : 83, 과학 : 93},
-  { 이름 : 'sue', 국어 : 94, 영어 : 94, 수학 : 84, 과학 : 94},
-] 
+const style = {
+  width: "500px",
+  height: "300px",
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center"
+}
+
 
 function App() {
- 
+  const handleChange = (event) =>{
+    console.log(event.target.name)
+  }
   return (
-    <table>
-      <tr>
-        {
-          Object.keys(scores[1]).map(key=>(
-            <th>{key}</th>
-          ))
-        }
-      </tr>
-      {scores.map(item =>
-          (
-          <tr>
-            {
-              Object.values(item).map(
-                value => (<td>{value}</td>)
-              )
-            }
-            
-          </tr>
-          )
-        )
-      }
-    </table>
+    <form>
+      <div>이름 : 
+        <input 
+          type="text" 
+          name="이름" 
+          value=""
+          onChange={handleChange}
+        /> 
+      </div>
+      <div>국어 : 
+        <input 
+          type="number" 
+          name="국어" 
+          value="" 
+          onChange={handleChange}
+          /> 
+        </div> 
+       <div>영어 : 
+        <input 
+          type="number" 
+          name="영어" 
+          value="" 
+          onChange={handleChange}
+          /> 
+        </div> 
+       <div>수학 : 
+        <input 
+          type="number" 
+          name="수학" 
+          value="" 
+          onChange={handleChange}
+          /> 
+        </div> 
+       <div>과학 : 
+        <input 
+          type="number" 
+          name="과학" 
+          value="" 
+          onChange={handleChange}
+          /> 
+        </div> 
+      <button>제출</button>
+    </form>
   )
 }
 
---
+++ src/components/ScoresTable.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+
+const scores = [
+  { 이름 : 'john', 국어 : 91, 영어 : 91, 수학 : 81, 과학 : 91},
+  { 이름 : 'peter', 국어 : 92, 영어 : 92, 수학 : 82, 과학 : 92},
+  { 이름 : 'susan', 국어 : 93, 영어 : 93, 수학 : 83, 과학 : 93},
+  { 이름 : 'sue', 국어 : 94, 영어 : 94, 수학 : 84, 과학 : 94},
+]
+
+function scoresTable() {
+  return (
+     <table>
+      <tr>
+        {
+          Object.keys(scores[1]).map(key=>(
+            <th>{key}</th>
+          ))
+        }
+      </tr>
+      {scores.map(item =>
+          (
+          <tr>
+            {
+              Object.values(item).map(
+                value => (<td>{value}</td>)
+              )
+            }
+          </tr>
+          )
+        )
+      }
+    </table>
+  )
+}
+
+export default scoresTable
